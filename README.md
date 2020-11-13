946368fa95822b93e5a1f723f43b036b5ad52e17
Niki Catapano <nsc9605@gmail.com>
2020-11-12 22:10:51 -0500

    added timer and fixed start button

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Credits
 
 https://www.youtube.com/watch?v=2Bpiluefkh8
+https://www.youtube.com/watch?v=pQr4O1OITJo&t=1398s
+https://www.youtube.com/watch?v=riDzcEQbX6k
+https://www.youtube.com/watch?v=6q3OpgJ-SHE
+
+https://www.w3schools..com/
--- script.js
+++ script.js
@@ -1,101 +1,11 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-  } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-  }
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
-function setup() {
-  noc
-}
-const startingMinutes = 2;
-let time = startingMinutes * 60;
-
-const timerEl = document.getElementById('timer');
-
-setInterval(updateTimer, 1000);
-
-function updateTimer() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
-  seconds = seconds < 2 ? '0' + seconds : seconds;
-
-  timerEl.innerHTML = $(minutes); $(seconds);
-  time--;
-}
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const timeCount = document.querySelector('.timer');
+const timeLine = document.querySelector('.header .time_line');
+const timer = document.getElementById('timer');
 
 const questions = [
   {
@@ -154,3 +64,131 @@ const questions = [
     ]
   }
 ]
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let shuffledQuestions, currentQuestionIndex;
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+
+// Which way to set timer for countdown???
+function startTimer(){
+  counter = setInterval(timer, 1000);
+  function timer(){
+      timeCount.textContent = time;
+      time--;
+      if(time < 9){
+        let addZero = timeCount.textContent;
+        timeCount.textContent = '0' + addZero;
+      }
+      if(time < 0){
+        clearInterval(counter);
+        timeCount.textContent = '00';
+      }
+  }
+}
+// // Or this way?
+// function startTimerLine(time){
+//   counterLine = setInterval(timer, 29);
+//   function timer(){
+//       time += 1;
+//       timeLine.style.width = time + 'px';
+//       // if(time < );
+//   }
+// }
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+})
+
+
+
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  startTimer()
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+  } else {
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+function setup() {
+  noc
+}
+
+
+// const timerEl = document.getElementById('timer');
+
+// setInterval(updateTimer, 1000);
+
+// function updateTimer() {
+//   const minutes = Math.floor(time / 60);
+//   let seconds = time % 60;
+
+//   seconds = seconds < 2 ? '0' + seconds : seconds;
+
+//   timerEl.innerHTML = $(minutes); $(seconds);
+//   time--;
+// }
+
