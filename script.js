27fe21129e923a3f77ad160bdcbd8682db25dea6
Niki Catapano <nsc9605@gmail.com>
2020-11-14 17:07:57 -0500

    updated javascript with score

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Define elements
-const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question');
-const answerButtonsElement = document.getElementById('answer-buttons');
-const timeCount = document.querySelector('.timer');
-const timeLine = document.querySelector('.header .time_line');
-const timer = document.getElementById('timer');
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const timeCount = document.querySelector(".timer");
+const timeLine = document.querySelector(".header .time_line");
+const timer = document.getElementById("timer");
 // const progress = document.getElementById('progress');
-const scoreContainer = document.getElementById('score-container');
-const rulesContainer = document.querySelector('.rules-container');
-const scoreCounter = document.getElementById('count');
+const scoreContainer = document.getElementById("score-container");
+const rulesContainer = document.querySelector(".rules-container");
+const scoreCounter = document.getElementById("count");
+const showHighScores = document.getElementById("highscores-btn");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -21,53 +22,49 @@ let shuffledQuestions, currentQuestionIndex;
 const startingMinutes = 2;
 let time = startingMinutes * 60;
 let count = 0;
+let timeLeft = [];
 
 // Which way to set timer for countdown???
-function startTimer(){
+function startTimer() {
   counter = setInterval(timer, 1000);
-  function timer(){
-      timeCount.textContent = time;
-      time--;
-      if(time < 9){
-        let addZero = timeCount.textContent;
-        timeCount.textContent = '0' + addZero;
-      }
-      if(time < 0){
-        clearInterval(counter);
-        timeCount.textContent = '00';
-      }
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeCount.textContent = "00";
+    }
   }
 }
-
-
 
 // HIDE INSTRUCTIONS CONTAINER WHEN START BUTTON IS CLICKED
 
 // let startButton = document.querySelector('start-btn');
 // let rulesContainer = document.querySelector('rules');
-// if (startButton.clicked) rulesContainer.style.display = 'hide';
-// else rulesContainer.style.display = null; 
 
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
+// There are 3 Event Listeners
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
-  
-
-})
-
+});
+highScoresButton.addEventListener('click', showHighScores);
 
 
 function startGame() {
-  startButton.classList.add('hide');
-  rulesContainer.classList.add('hide');
-  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  startButton.classList.add("hide");
+  rulesContainer.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
-  questionContainerElement.classList.remove('hide');
+  questionContainerElement.classList.remove("hide");
   startTimer();
   setNextQuestion();
-  renderCounter();
-};
+  // renderCounter();
+}
 
 function setNextQuestion() {
   resetState();
@@ -76,87 +73,112 @@ function setNextQuestion() {
 
 function showQuestion(question) {
   questionElement.innerText = question.question;
-  question.answers.forEach(answer => {
-    const button = document.createElement('button');
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
     button.innerText = answer.text;
-    button.classList.add('btn');
+    button.classList.add("btn");
     if (answer.correct) {
       button.dataset.correct = answer.correct;
-      
     }
-    button.addEventListener('click', selectAnswer);
+    button.addEventListener("click", selectAnswer);
     answerButtonsElement.appendChild(button);
-  })
+  });
 }
 
 // ADD LOCAL STORAGE TO HOLD SCORES
 
 function resetState() {
   clearStatusClass(document.body);
-  nextButton.classList.add('hide');
+  nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
-
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
+  if (correct === "true") {
+    count++;
+  }
   setStatusClass(document.body, correct);
-  Array.from(answerButtonsElement.children).forEach(button => {
+  Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
-  })
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide');
+    nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = 'Restart'; // bring used to container with high scores and option to restart
-    startButton.classList.remove('hide');
+    endGame();
+    // startButton.innerText = 'Restart'; // bring used to container with high scores and option to restart
+    startButton.classList.remove("hide");
   }
 }
 
-function renderCounter(){
-  if(count <= time){
-    scoreCounter.textContent = count;
-  count++;
-  }else{
-  count = 0;
-  }
+function endGame() {
+  hideQuestionsContainer(document.body);
+  nextButton.classList.add("hide");
 }
 
+// // checkAnswer(event) {
+//   console.log(questions[currentQuestion].correctAnswer===event.target.id);
+//   if (questions[currentQuestion].correctAnswer===event.target.id){
+//     currentQuestion++;
+//     if (currentQuestion < questions.length){
+//       displayQuestion()
+//     }
+//     else{
+//       showHighScores();
+//     }
+//   }
+
+
+[{
+  initials: "DJK",
+  score: 5
+}]
+
+function showHighScores(){}
+
+// function renderCounter(){
+//   if(count <= time){
+
+//   count++;
+//   } else {
+//   count = 0;
+
+//   }
+// }
 
 function setStatusClass(element, correct) {
   clearStatusClass(element);
   if (correct) {
-    
     // count++;
-    element.classList.add('correct');
-    count++;
-   
+    element.classList.add("correct");
+
+    scoreCounter.textContent = count;
   } else {
-    
-    element.classList.add('wrong');
-
+    element.classList.add("wrong");
+    seconds = seconds - 10;
   }
-}
-
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+  }
+    else {
+      timeLeft -= 10
+    }
+  }
 
 
 function clearStatusClass(element) {
-  element.classList.remove('correct');
-  element.classList.remove('wrong');
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
 
 function setup() {
-  noc
+  noc;
 }
 
 // TO DO: add a function for high scores: a place to write initials, store locally as well as reset after last question
-
-
-
-
-
 
 // const timerEl = document.getElementById('timer');
 
@@ -171,7 +193,6 @@ function setup() {
 //   timerEl.innerHTML = $(minutes); $(seconds);
 //   time--;
 // }
-
 
 // // Or this way?
 // function startTimerLine(time){
